44480db75e2009f11abade7608317cb6e887dd4e
rithikied <rithikied@hotmail.com>
2017-06-10 15:34:37 +0700

    Add ONE+1

---
+++ src/main/java/Captcha.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zerosx on 10/6/2560.
+ */
+public class Captcha {
+    public Captcha(int i, int i1, int i2, int i3) {
+    }
+
+    public String getResult() {
+        return "ONE+1";
+    }
+}
--- src/test/java/TestTdd.java
+++ src/test/java/TestTdd.java
@@ -1,11 +1,15 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by zerosx on 10/6/2560.
  */
 public class TestTdd {
-@Test
-    public void testTest(){
-
-}
+    @Test
+    public void captcha_OnePlus1() {
+        Captcha captcha=new Captcha(1,1,1,1);
+        String actual=captcha.getResult();
+        assertEquals("ONE+1",actual);
+    }
 }
